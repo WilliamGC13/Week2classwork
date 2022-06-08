8cda520c6f9b940a10ff084156a4d3624c31aa88
WilliamGC13 <coffeywg@outlook.com>
2022-06-07 21:32:10 -0400

    1st classwork commit

---
+++ js-week-2/index.js
@@ -0,0 +1,65 @@
+// Make a loop that console.log even numbers from 1-10
+
+
+// var age = prompt("what is your age?")
+// var firstName = prompt("what is your name?")
+// var isHungry = confirm("are you hungry?")
+// alert(firstName + " hungry? " + isHungry)
+
+// console.log(age);
+// console.log(isHungry);
+
+// for(i = 1; i<= 5; i++) {
+//     console.log(i)
+
+// }
+
+// var age = 20
+
+// if(age >=15 && age <=18) {
+//     alert("you can take your drining test")
+// } else if( age > 18) {
+//     alert("you can drive")
+// } else {
+//     alert("you can't drive")
+// }
+
+// switch(true) {
+//     case age >=15 && age <=18:
+//         alert("you can take your driving test")
+//         break;
+//     case age > 18:
+//         alert ("you can drive")
+//         break;
+//     defailt:
+//         alert("you cant drive")
+// }
+
+// var question =  confirm("would you like to start?");
+// var list = [];
+
+// while(list.length < 5 && question === true) {
+//     alert("starting app");
+//     var colorQuestion = prompt("give me a color");
+//     list.push(colorQuestion);
+
+//     if(list.length === 5){
+//         console.log(list)
+//     }
+//  }
+
+// var count = 0;
+
+
+// do {
+//     count++
+//     console.log(count)
+// } while(count < 3)
+
+
+
+// for(i = 0; i <= 10; i++) {
+//     if(i % 2 === 0) {
+//         console.log(i);
+//     }   
+// }
